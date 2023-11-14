f2c4a53f045ae607ea393daa802a82dad1f27870
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-11-14 20:50:12 +0600

    Lab 10 task 4

--- Lab_10/210042174_T04L10B.cpp
+++ Lab_10/210042174_T04L10B.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -28,4 +29,79 @@ Node *insert(Node *root, int data)
     }
 
     return root;
+}
+
+void findPath(Node *root, int x, int y, vector<int> &path)
+{
+    while (root != nullptr)
+    {
+        path.push_back(root->data);
+
+        if (root->data > y)
+        {
+            root = root->left;
+        }
+        else if (root->data < x)
+        {
+            root = root->right;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+int countNodesInPath(const vector<int> &path, int x, int y)
+{
+    int count = 0;
+    for (int node : path)
+    {
+        count++;
+        if (node == x || node == y)
+        {
+            break;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    Node *root = nullptr;
+    int num;
+
+    while (true)
+    {
+        cin >> num;
+        if (num == -1)
+        {
+            break;
+        }
+        root = insert(root, num);
+    }
+
+    while (true)
+    {
+        int x, y;
+        cin >> x >> y;
+        if (x == -1 || y == -1)
+        {
+            break;
+        }
+
+        vector<int> path;
+        findPath(root, x, y, path);
+
+        for (int node : path)
+        {
+            cout << node << " ";
+        }
+        cout << endl;
+
+        int distance = countNodesInPath(path, x, y);
+        cout << distance << endl;
+    }
+
+    return 0;
 }
